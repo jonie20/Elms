54c953e85579a8f573037aac4e85f6d41861e3da
Moti-Jnr <kipchimoti@gmail.com>
2025-01-19 16:50:13 +0300

    groups

--- user/views.py
+++ user/views.py
@@ -1,9 +1,17 @@
 from datetime import datetime, timedelta
+
+from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.models import Group
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.views import View
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
